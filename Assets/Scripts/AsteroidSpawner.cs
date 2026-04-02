e1caf34c85eac10e8bc516ec3e45f9cfce62b79d
Ras T <erenceliktener@gmail.com>
2026-04-03 00:40:01 +0300

    feat: add asteroid homing behavior

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -4,16 +4,19 @@ public class AsteroidSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject asteroidPrefab;
     [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private Transform playerTarget;
 
     public void SpawnAsteroids()
     {
         for (int i = 0; i < spawnPoints.Length; i++)
         {
-            Instantiate(
+            GameObject asteroid = Instantiate(
                 asteroidPrefab,
                 spawnPoints[i].position,
                 spawnPoints[i].rotation
             );
+
+            asteroid.GetComponent<AsteroidHoming>().SetTarget(playerTarget);
         }
     }
 }
